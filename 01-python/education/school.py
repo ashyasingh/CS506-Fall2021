5df580858b0416ef10ae602eabccbd78e1bf5117
Ashya Singh <ashyasingh@nat-wireless-guest-reg-153-50.bu.edu>
2021-09-27 17:04:14 -0400

    i redid the school.py file and edited all on a new branch i made from the master so hopefully the conflicts are resolved

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,8 @@
 def draw_school():
-    print("school not found")
-    return
+	print("this is stony brook middle school")
+	print("we have 6th, 7th, and 8th grade")
+	print("you can be on 1 of 3 teams in each grade")
+	print("we're located in westford, mass")
+	print("100% of abbott elementary school and 50% of day elementary school go to stony brook")
+	print("we're brand new!") 
+	return
